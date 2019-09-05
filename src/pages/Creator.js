b858808d3df57af975284e0c0921419474d05449
Dan <dan.kimhaejun@gmail.com>
2019-09-05 20:32:29 +0900

    creator basic create

--- src/pages/Creator.js
+++ src/pages/Creator.js
@@ -1,13 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import './Creator.css';
 
 class Creator extends Component {
-  render() {
-    return (
-        <div>
-          this is Creator
-        </div>
-    );
-  }
+	render() {
+		const creators = [ '강태훈', '육광휘', '김해준' ];
+		const creatorsImg = creators.map((ele,index) => (
+			<div className="creators" key={ele + index}>
+				<img
+					className="creatorsPic"
+					src="//upload.wikimedia.org/wikipedia/commons/thumb/5/50/Albert_Einstein_%28Nobel%29.png/100px-Albert_Einstein_%28Nobel%29.png"
+				/>
+				<br />
+				{ele}
+			</div>
+		));
+		return (
+			<div>
+				<br />
+				<h3>Hello, Writer</h3>
+				<p>by codestates</p>
+				<div id="usersInfo">
+					<img
+						className="usersPic"
+						src="//upload.wikimedia.org/wikipedia/commons/thumb/5/50/Albert_Einstein_%28Nobel%29.png/100px-Albert_Einstein_%28Nobel%29.png"
+					/>
+					<div>
+						유저이름
+						<br />
+						<br />
+            Runaway trains
+            <br />
+						<div>{creatorsImg}</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Creator;
