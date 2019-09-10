d3a1a3cb8445c11b3222d38709fea60fe7e7144d
육광휘 <khwi@yuggwanghwiui-MacBookPro.local>
2019-09-10 21:06:15 +0900

    favicon change and put background img

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,20 +1,25 @@
 /* eslint-disable no-tabs */
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './Welcome.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Welcome.css";
+import logo from "./img/logo.png";
 
 class Welcome extends Component {
-	render() {
-		return (
-			<div className="welcomePage">
-				<h1 id="welcomeWrite">Hello, Writer.</h1>
-				<div>
-					<Link className="welcomeBtn" to="/signup">Sign up</Link>
-					<Link className="welcomeBtn" to="/signin">Sign in</Link>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="welcomePage">
+        <img src={logo} alt="" />
+        <div>
+          <Link className="welcomeBtn" to="/signup">
+            Sign up
+          </Link>
+          <Link className="welcomeBtn" to="/signin">
+            Sign in
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Welcome;
